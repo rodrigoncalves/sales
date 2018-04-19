e60f4a091fa60c655f2ebcc1b7d73e6955332ef5
Rodrigo Santanta Gonçalves <rodrigosg2000@gmail.com>
2018-04-19 15:19:23 +0000

    show fields in rails_admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -4,7 +4,7 @@ RailsAdmin.config do |config|
 
   ## == Devise ==
   config.authenticate_with do
-  warden.authenticate! scope: :user
+    warden.authenticate! scope: :user
   end
   config.current_user_method(&:current_user)
 
@@ -23,19 +23,102 @@ RailsAdmin.config do |config|
   ## To disable Gravatar integration in Navigation Bar set to false
   # config.show_gravatar = true
 
-  config.actions do
-    dashboard                     # mandatory
-    index                         # mandatory
-    new
-    export
-    bulk_delete
-    show
-    edit
-    delete
-    show_in_app
+  config.model Sale do
+    create do
+      field :client
+      field :sale_date
+      field :discount
+      field :notes
+      field :product_quantities
 
-    ## With an audit adapter, you can add:
-    # history_index
-    # history_show
+      field :user_id, :hidden do
+        default_value do
+          bindings[:view]._current_user.id
+        end
+      end
+    end
+
+    edit do
+      field :client
+      field :sale_date
+      field :discount
+      field :notes
+      field :product_quantities
+
+      field :user_id, :hidden do
+        default_value do
+          bindings[:view]._current_user.id
+        end
+      end
+    end
+  end
+
+  config.model Client do
+    create do
+      field :name
+      field :company_name
+      field :document
+      field :email
+      field :phone
+      field :notes
+      field :status
+      field :address
+
+      field :user_id, :hidden do
+        default_value do
+          bindings[:view]._current_user.id
+        end
+      end
+    end
+
+    edit do
+      field :name
+      field :company_name
+      field :document
+      field :email
+      field :phone
+      field :notes
+      field :status
+      field :address
+
+
+      field :user_id, :hidden do
+        default_value do
+          bindings[:view]._current_user.id
+        end
+      end
+    end
+
+    list do
+      field :name
+      field :company_name
+      field :document
+      field :email
+      field :phone
+      field :notes
+      field :status
+      field :address
+    end
+  end
+
+  config.model ProductQuantity do
+    visible false
+  end
+
+  config.model Address do
+    visible false
+  end
+
+  config.model ProductQuantity do
+    edit do
+      field :product
+      field :quantity
+
+      field :user_id, :hidden do
+        default_value do
+          bindings[:view]._current_user.id
+        end
+      end
+    end
   end
 end
