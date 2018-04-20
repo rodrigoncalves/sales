690c2f4dd6a9c5ba48acb18cbd9b951bfbf92280
Rodrigo Santanta Gonçalves <rodrigosg2000@gmail.com>
2018-04-20 21:16:34 +0000

    login page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable,
+         #:registerable,
+         #:recoverable,
+         :rememberable,
+         :trackable,
+         :validatable
   enum kind: [:salesman, :manager]
   enum status: [:active, :inactive]
   has_many :comissions
